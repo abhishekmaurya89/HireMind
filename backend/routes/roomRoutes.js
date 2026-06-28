1092d91447b76747bbf428e5e22bf36cdb06f32d
maurya72 <abhishekmaurya8936@gmail.com>
2026-06-28 09:43:02 +0530

    fix: correct import path for RoomController in roomRoutes

--- backend/routes/roomRoutes.js
+++ backend/routes/roomRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import { createRoom } from "../controllers/roomController.js";
+import { createRoom } from "../controllers/RoomController.js";
 const router = express.Router();
 router.post("/create", auth, createRoom);
 export default router;
+ 
